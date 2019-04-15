08213843d387e2b0cf77464c96421a4680588c27
Arthur de Morais Novaes <arthurnovaes0@gmail.com>
2019-04-15 16:16:29 -0300

    pegando o mac do serve via protocolo arp

--- client.sh
+++ client.sh
@@ -3,17 +3,31 @@
 echo "Digite a mensagem que você deseja enviar" 
 read MENSAGEM
 echo "Digite o IP do destino"
-read IP
+read IPSERVER
 
-echo
-MacOrigem=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
-echo "MacAddress Cliente: $MacOrigem"
+# ---------------------------------------------
+# PEGA O MAC DO CLIENTE
+MACCLIENTE=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
 
-# solicita o MAC do endereço de IP
-# MACDESTINO=$(cat solicitaMAC.txt | netcat 172.16.252.72 1234)
+
+# ---------------------------------------------
+# SOLICITA O MAC DO SERVER
+
 # Perguntia via protocolo ARP qual o endereço fisico do IP informado
-arp 192.168.1.1
-MACDESTINO=$(arp 192.168.1.1 | awk {print $5}))
+MACSERVER=$(arp 172.16.128.1 | awk 'FNR==2{ print $3 }')
+
+# outra maneira de fazer sem o protocolo ARP
+# MACDESTINO=$(cat solicitaMAC.txt | netcat 172.16.252.72 1234)
+
+
+# ---------------------------------------------
+# ENVIA O DADO PARA O DESTINO
+echo
+echo "MAC do Cliente: $MACCLIENTE"
+echo "MAC do Server: $MACSERVER"
+echo "IP Server: $IPSERVER"
+echo "Mensagem: $MENSAGEM"
+
 
 # METODO 1 - O cliente recebe o arquivo do servidor e salva em um arquivo
 # netcat 172.16.252.72 1234 > pdu_Cliente.txt
@@ -21,5 +35,4 @@ MACDESTINO=$(arp 192.168.1.1 | awk {print $5}))
 # METODO 2 - O cliente manda o aquivo para o IP do servidor
 # cat quadro.txt | netcat 172.16.252.72 1234
 
-MACSERVER=$(netcat 172.16.252.72 1234)
-echo "MAC do Server: $MACSERVER"
+# MACSERVER=$(netcat 172.16.252.72 1234)
