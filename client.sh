a123b4846dc2b390aa39aabc9a7d4d5d006d0af9
Arthur de Morais Novaes <arthurnovaes0@gmail.com>
2019-04-15 13:25:25 -0300

    fazendo a requisição do MAC do server

--- client.sh
+++ client.sh
@@ -4,11 +4,19 @@ echo "Digite a mensagem que você deseja enviar"
 read MENSAGEM
 echo "Digite o IP do destino"
 read IP
-MAC=$(cat /sys/class/net/enp1s0f1/address)
-echo "$MAC"
+
+echo
+MacOrigem=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
+echo "MacAddress: $MacOrigem"
+
+# solicita o MAC do endereço de IP
+MACDESTINO=$(cat solicitaMAC.txt | netcat 172.16.252.72 1234)
 
 # METODO 1 - O cliente recebe o arquivo do servidor e salva em um arquivo
 # netcat 172.16.252.72 1234 > pdu_Cliente.txt
 
 # METODO 2 - O cliente manda o aquivo para o IP do servidor
-cat quadro.txt | netcat 172.16.252.72 1234
+# cat quadro.txt | netcat 172.16.252.72 1234
+
+MACSERVER=$(netcat 172.16.252.72 1234)
+echo "MAC do Server $MACSERVER"
--- serve.sh
+++ serve.sh
@@ -12,9 +12,19 @@
 # done <&${COPROC[0]} >&${COPROC[1]}
 
 
+# armazena o MAC do server em um arquivo
+cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address > MACServer.txt
 
 # METODO 1 - O cliente recebe o arquivo do servidor e salva em um arquivo
 # cat quadro.txt | nc -l -p 1234
 
 # METODO 2 - O cliente manda o aquivo para o IP do servidor
+RECEBE=$(nc -l -p 1234 -q 1)
+echo "Recebido: $RECEBE"
+case "$RECEBE" in
+		MAC)	cat MACServer.txt | nc -l -p 1234;;
+		*) 		echo "Não entrou"
+esac
+
+
 nc -l -p 1234 -q 1 > quadroServe.txt < /dev/null
