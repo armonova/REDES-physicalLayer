eb9a2c8da1d3991f56c6a84bf5b1473fbdf5c215
Tiago Araújo <tiago.araujo.neo@gmail.com>
2019-05-06 02:09:41 -0300

    Nomes para gerar e ler arquivos

---
+++ CLIENT-open-PDU.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATA=`cat SERVER1-CLIENTE1-quadro.txt`
+
+echo $DATA
+
+echo $DATA >> CLIENTE1-CLIENTE4-mensagem.txt
---
+++ SERVER-open-PDU.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATA=`cat CLIENTE1-SERVER1-quadro.txt`
+
+echo $DATA
+
+echo $DATA >> SERVER1-SERVER4-mensagem.txt
